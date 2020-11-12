f53e4c831b5368da210da2fdf0db65561d04c794
JavidNoshadi <javid.king63@gmail.com>
2020-11-13 02:06:02 +0330

    i ha8 css

--- public/script.js
+++
@@ -1,9 +0,0 @@
-
-
-const delBtn = document.querySelector('.delete-btn')
-delBtn.addEventListener('click',Delete_btn)
-function Delete_btn(){
-    let dialogbox = document.querySelector('.delete-dialog-box')
-    dialogbox.classList.toggle('open')
-    console.log('toggling ')
-}
