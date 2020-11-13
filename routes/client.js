48010c0e854468ab7473e0dbe9085fd7bdfa466e
JavidNoshadi <javid.king63@gmail.com>
2020-11-13 21:31:24 +0330

    h

--- routes/client.js
+++ routes/client.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
-
+const User = require('../config/User.js')
 router
 .get('/',(req,res)=>{
     res.render('home')
+})
+.get('/user/dashboard', (req,res)=>{
+    res.render('userDashboard')
 })
 .get('/login',(req,res)=>{
     res.render('login')
@@ -11,5 +14,39 @@ router
 .get('/signup',(req,res)=>{
     res.render('signup')
 })
+.post('/login', async (req,res)=>{
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    },(succ,err)=>{
+        if(err){
+            res.render('userDashboard')
+        }else{
+            res.render('login',{
+                msg: `رمز عبور اشتباه است.`
+            })
+        }
+    })
+})
 
+.post('/signup', async(req,res)=>{
+    const user = await User.findOne({email: req.body.email},(succ,err)=>{
+        if(err){
+            res.render('signup',{
+                msg: 'شما قبلا ثبت نام کرده اید.'
+            })
+        }else{
+         if(req.body.password != req.body.password2){
+             res.render('signup',{
+                 msg: 'رمزهای عبور همخوانی ندارند.'
+             })
+         }else{
+          User.create(req.body)
+             res.render('userDashboard',{
+                 msg: 'ثبت نام با موفقیت انجام شد.'
+           })
+         }
+        }
+    })
+})
 module.exports = router
